12c9ec62622ad96aed584e6030190105de42e5e3
atulsingh-02 <atulsingh@Atuls-MacBook-Air.local>
2022-11-04 19:15:19 +0530

    adding git assigent

---
+++ pr1.js
@@ -0,0 +1,17 @@
+let num = 13;
+    let counter = 0;
+    for(i = 1; i <= num; i++) {
+        
+        if(num % i == 0) {
+            counter++;
+        }
+       
+    }
+     
+      
+     if(counter !== 2) {
+         console.log("Yes");
+     }
+     else {
+         console.log("No");
+     }
